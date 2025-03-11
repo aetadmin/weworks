f8af37201212bff6f9dba2ed4e6443c67932b833
WeWorks Developer <user@weworks.com>
2025-03-11 11:29:15 +1100

    Initial commit of WeWorks project

---
+++ apps/client/%40/shadcn/components/app-sidebar.tsx
@@ -0,0 +1,7 @@
+import { useTranslation } from "react-i18next";
+
+const { t, lang } = useTranslation("weworks");
+
+name: "WeWorks",
+
+<span className="truncate font-semibold text-xl">WeWorks</span> 
